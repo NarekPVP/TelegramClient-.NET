9effd08948e0a9e4e3a9b1f0fc888350aab5d196
NarekPVP <hnarek2005@gmail.com>
2023-04-23 20:34:27 +0400

    Add project files.

---
+++ TelegramAPI/TelegramClient.cs
@@ -0,0 +1,62 @@
+﻿namespace TelegramAPI
+{
+    public class TelegramClient : IDisposable
+    {
+        /*
+         curl
+        -X POST "https://api.telegram.org/bot<your_bot_token>/auth/sendCode"
+        -d "phone_number=<your_phone_number>&amp;settings={\"is_bot\":false,\"allow_flashcall\":false,\"current_number\":\"\",\"api_id\":<your_api_id>,\"api_hash\":\"<your_api_hash>\",\"lang_code\":\"en\"}"
+         */
+
+        public string ClientName { get; set; }
+        public string? ApiHash { get; set; }
+        public int? ApiId { get; set; }
+
+        private bool disposed = false;
+
+        public TelegramClient(string clientName)
+        {
+            ClientName = clientName;
+        }
+
+        public TelegramClient(string clientName, string apiHash, int apiId)
+        {
+            ClientName = clientName;
+            ApiHash = apiHash;
+            ApiId = apiId;
+        }
+
+        public async Task Init()
+        {
+            if (ApiHash == null || ApiId == null) 
+                return;
+
+            using (var client = new HttpClient())
+            {
+                using var result = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/2");
+                Console.WriteLine("Status: " + result.StatusCode);
+                await Console.Out.WriteLineAsync(await result.Content.ReadAsStringAsync());
+                
+                result.Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~TelegramClient()
+        {
+            Dispose(false);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // ...
+        }
+
+        public override string ToString() => $"Client: {ClientName}\nApi Hash: {ApiHash}\nApi Id: {ApiId}";
+    }
+}
---
+++ TelegramAuth/Program.cs
@@ -0,0 +1,25 @@
+﻿using TelegramAPI;
+
+namespace TelegramAuth;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        TelegramClient client = new TelegramClient(
+            clientName: Guid.NewGuid().ToString(),
+            apiHash: "608684bc8defbee867c2cfc12b798c9f",
+            apiId: 21688496);
+
+        Console.WriteLine(client);
+
+        await Start(client);
+
+        client.Dispose();
+    }
+
+    static async Task Start(TelegramClient client)
+    {
+        await client.Init();
+    }
+}
